f68bf4842edebe72773866f47cea905a43e4a8aa
leegyeongwhan <leekhy02@naver.com>
2022-11-06 18:10:26 +0900

    Db설정 앤티티 패키지 리포지토리 패키지 생성,테스트

--- src/main/java/study/datajpa/entity/Member.java
+++ src/main/java/study/datajpa/entity/Member.java
@@ -1,4 +1,27 @@
 package study.datajpa.entity;
 
+import lombok.Getter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
 public class Member {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String userName;
+
+    protected Member() {
+
+    }
+
+    public Member(String name) {
+        this.userName = name;
+    }
+
+
 }
---
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -0,0 +1,14 @@
+package study.datajpa.repository;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+@Rollback
+class MemberRepositoryTest {
+
+}
