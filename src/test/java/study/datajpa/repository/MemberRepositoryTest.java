8f98af2a723d73efa35897c19c8fcf6970722fd7
leegyeongwhan <leekhy02@naver.com>
2022-11-08 03:18:31 +0900

    feat(DataJpa Test작성): Data Jpa를 통해 멤버를 테스트

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -1,8 +1,12 @@
 package study.datajpa.repository;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -10,5 +14,21 @@ import static org.junit.jupiter.api.Assertions.*;
 @Transactional
 @Rollback
 class MemberRepositoryTest {
+
+    @Autowired
+    MemberRepository memberRepository;
+
+    @Test
+    public void testMember() {
+        Member member = new Member("memberA");
+        Member svaveMember = memberRepository.save(member);
+
+        Member findMember = memberRepository.findById(svaveMember.getId()).get();
+        Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
+        Assertions.assertThat(findMember.getUserName()).isEqualTo(member.getUserName());
+        Assertions.assertThat(findMember).isEqualTo(member);
+
+
+    }
 
 }
